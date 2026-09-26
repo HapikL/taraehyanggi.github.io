59ba95daa0497aa4faa9141aa12ef8593f2d897e
HapikL <otlcu0913@gmail.com>
2026-09-27 01:02:31 +0900

    Update script.js

--- script.js
+++ script.js
@@ -527,24 +527,6 @@ dayHeaderElement.classList.add('day-header');
 const numberElement = document.createElement('div');
 numberElement.classList.add('day-number');
 numberElement.textContent = displayDay;
-        
-const holidayName = koreanHolidays[dateString];
-
-if (holidayName) {
-    dayElement.classList.add('holiday-cell');
-
-    const holidayElement =
-        document.createElement('div');
-
-    holidayElement.classList.add('holiday-name');
-
-    holidayElement.textContent =
-        holidayName;
-
-    numberElement.classList.add('holiday-number');
-
-    dayElement.appendChild(holidayElement);
-}
 
 /* 해당 날짜의 A/B 일정 존재 여부 */
 
